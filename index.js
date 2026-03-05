546d6201af74981076b1e9d722e57eeb1f97bd14
Roger <rogerplaballus@gmail.com>
2026-03-05 11:55:25 +0100

    Fix Express 5 wildcard route - use middleware instead

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-module.exports = express()
-  .use(cors())
-  .use(express.json())
-  .use(express.static(path.join(__dirname)))
-  .get('*', (req, res) => {
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html for all non-file requests (SPA fallback)
+app.use((req, res, next) => {
+  if (!path.extname(req.path)) {
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
+  } else {
+    next();
+  }
+});
+
+module.exports = app;
